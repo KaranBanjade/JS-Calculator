6a927f452596c4b7c30be562dac45e866402fa58
Karan Banjade <karan.banjade@gmail.com>
2021-08-01 16:15:35 +0530

    "Adding the copy button"

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ let Minus = document.getElementById("Minus");
 let Product = document.getElementById("Product");
 let Divide = document.getElementById("Divide");
 let Clear = document.getElementById("Clear");
+let Copy = document.getElementById("copy");
 
 let Out = document.querySelector("#Result");
-// Output.
 
 let x = 0;
 let y = 0;
@@ -33,12 +33,24 @@ function div(){
     Get();
     Out.value = x/y;
 }
-
+// Function to clear all data
 Clear.addEventListener('click',()=>{
     In1.value = "";
     In2.value = "";
     Out.value = "";
 })
+
+// Function to copy value of Output
+Copy.addEventListener('click',()=>{
+    /* Get the text field */
+    var copyOut = document.getElementById("Result");
+    /* Select the text field */
+    copyOut.select();
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    /* Alert the copied text */
+    alert("Copied the Result: " + copyOut.value);
+  })
 Add.addEventListener('click',add);
 Minus.addEventListener('click',sub);
 Product.addEventListener('click',mul);
